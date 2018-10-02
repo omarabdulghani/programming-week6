6c6bf7917875b682403d7e8007edd4d8d2461b1a
omarabdulghani <42969200+omarabdulghani@users.noreply.github.com>
2018-10-02 22:56:53 +0200

    first try

---
+++ Assignment 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[20];
+            string numbersText = "";
+            // fill array with numbers
+           
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int value = rnd.Next(0, 200);
+                
+                + numbersText += numbers[i] + " ";
+
+            }
+
+            Console.WriteLine(numbersText);
+
+            Console.ReadKey();
+        }
+    }
+}
